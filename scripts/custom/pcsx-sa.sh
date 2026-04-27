13f3a66947cca3c05b80a297a51a7cebff43e6b7
cobaltgit <cobaltsecuremail312@proton.me>
2026-04-27 21:58:39 +0100

    pcsx-sa: pull out all the stops
    Bloody Roar 2 now runs at full 60fps at 1512mhz

--- scripts/custom/pcsx-sa.sh
+++ scripts/custom/pcsx-sa.sh
@@ -8,6 +8,40 @@ git clone --recursive https://github.com/notaz/pcsx_rearmed.git pcsx_rearmed
 cd pcsx_rearmed
 git apply /scripts/custom/patches/pcsx_rearmed_trimuismart.patch
 
+export CFLAGS="-Ofast --sysroot=$SYSROOT \
+    -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard \
+    -marm \
+    -pipe \
+    -fno-stack-protector \
+    -fno-pic \
+    -fno-common \
+    -fno-ident \
+    -fno-plt \
+    -funroll-loops \
+    -frename-registers \
+    -fsection-anchors \
+    -fsched-pressure \
+    -fipa-pta \
+    -fipa-cp \
+    -fipa-cp-clone \
+    -findirect-inlining \
+    -fprefetch-loop-arrays \
+    -falign-functions=16 \
+    -falign-loops=16 \
+    -ffunction-sections \
+    -fdata-sections \
+    -fuse-linker-plugin \
+    -flto=auto"
+
+export LDFLAGS="--sysroot=$SYSROOT -L$SYSROOT/usr/lib \
+    -Wl,--gc-sections \
+    -Wl,--strip-all \
+    -Wl,--as-needed \
+    -Wl,--allow-multiple-definition \
+    -Wl,-O1 \
+    -fuse-linker-plugin \
+    -flto=auto"
+
 CFLAGS="$CFLAGS -DMIYOO" ./configure \
     --disable-dynamic \
     --enable-threads \
@@ -19,6 +53,8 @@ CFLAGS="$CFLAGS -DMIYOO" ./configure \
 make -j$(nproc)
 
 mkdir -p /output/pcsx/lib
+
+${CROSS}-strip --strip-all pcsx
 
 cp pcsx /output/pcsx/
 cp -r frontend/320240/skin/ /output/pcsx/
